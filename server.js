b22052471676c598c68f601f8bf90132dcd978ae
Glitch (url-shorten-microservice-ajb) <none>
2018-05-21 09:09:54 +0000

    🎓🍰 Checkpoint ./server.js:1080052/111

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // init project
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
+
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
