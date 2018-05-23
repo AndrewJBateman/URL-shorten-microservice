d6ed3816af3c62efe3d7a64c6b448c7fc68ac3e0
Glitch (url-shorten-microservice-ajb) <none>
2018-05-23 07:51:55 +0000

    🌄🌃 Checkpoint ./server.js:1080052/646

--- server.js
+++ server.js
@@ -14,28 +14,15 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-//when a GET request is made to the homepage, respond with the index.html file, 
-//If error, log an error, else log 'sent'
-app.get('/', (req,res) => {
-  var fileName = path.join(__dirname, 'index.html');
-  res.sendFile(fileName, (err) => {
-    if (err){
-     console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-     console.log('Sent:', fileName); 
-    }
-  });
-});
+app.use(express.static(__dirname, +'/public'));
 
 //Get to obtain longURL as entry for database (* means accept all the url)
 app.get('/new/:longURL(*)', (req, res, next) => {
   var { longURL } = req.params;
   
-  //use regex to check url is valid
-  // this regex info from http://stackoverflow.com/questions/
-  const regex = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
+  //use regex to check url is validrom http://stackoverflow.com/questions/
+  const regex = /^((ftp|http|https
+  // this regex info f):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
 
   if (regex.test(longURL)===true) {
      return res.json({longURL});
