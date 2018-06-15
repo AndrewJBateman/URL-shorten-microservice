07759673e998b823b2311f43c450739721b95dd1
Glitch (url-shorten-microservice) <none>
2018-06-15 12:46:34 +0000

    Updated to meet new FCC API cert

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -5,11 +6,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const shortURL = require('./models/shortURL');
+const url = require('url');
+const dns = require('dns'); 
+const dotenv = require('dotenv').config();
 const shortid = require('shortid'); //alphanumeric characters only, all url -friendly
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
 const port = process.env.PORT || 3000;
+const httpRegex = /^(?:f|ht)tps?\:\/\//
 const regex = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -28,43 +34,45 @@ mongoose.connect(process.env.MONGODB_URI, function(err, db){
   });
   
   //Get to obtain original URL as entry for database (* means accept all the url)
-  app.get('/new/:originalURL(*)', (req, res) => {
-    const { originalURL } = req.params;
-    //use regex to check url is valid, regex const taken from http://stackoverflow.com/questions/   
-    if (regex.test(originalURL)===true) { //save as constant in database
-      const data = new shortURL({
-        originalURL: originalURL,
-        shortenedURL: shortid.generate()
-      }); //end const data
-      console.log(data); //works
+  app.post('/api/shorturl/new', (req, res) => {
+    const originalURL = req.body.url;
+    //add http if not there already using prependHttp function
+    const parsedURL = url.parse(prependHttp(originalURL));
+    console.log(parsedURL) //shows full deails of url
+    
+    //use dns lookup function to check url is valid
+    dns.lookup(parsedURL.host, (err, address) => {
+      if (address === undefined) {
+        res.json({ error: 'This url failed the formatting test - check and try again. ' +err });
+      } else {
+          const data = new shortURL({
+            originalURL: prependHttp(originalURL),
+            shortenedURL: shortid.generate()
+          }); //end const data
+          console.log(data); //works
       
-      data.save(err => {
-        if(err) {
-          console.log(err);
-          return res.send('error: unable to save to database');
-        }
-        console.log('all OK');
-      }); //end of function save error
-      res.send(data);
-    } //end of regex if condition, else return error
-    else{
-      return res.json //if url is not the correct format
-      ({ 
-      originalURL: 'failed the formatting test - try another URL' //works
-      });
-    } //end else
-  }); //end function get
+          data.save(err => {
+            if(err) {
+              console.log(err);
+              return res.send('error: unable to save to database');
+            }
+            console.log('all OK');
+          }); //end of function save error
+          res.send(data);
+      } //end of else
+    }); //end of dns if condition, else return error
+  }); //end function post
   
   //Query Horuko-mLab database and return original URL
-  app.get('/:shortURLID', (req, res, next) => {
+  app.get('/api/shorturl/:shortURLID', (req, res, next) => {
     var short = req.params.shortURLID; //store param value obtained from user input
     shortURL.findOne({'shortenedURL': short}, (err, data) => {
       if(err) throw err;
     
       if(data){
-        var regex = new RegExp("^(http|https)://", "i");
+        var httpRegex = new RegExp("^(http|https)://", "i");
         var stringToCheck = data.originalURL; 
-        if (regex.test(stringToCheck)){ //if test passes redirect to the original URL
+        if (httpRegex.test(stringToCheck)){ //if test passes redirect to the original URL
           res.redirect(301, data.originalURL);
         } else { //otherwise add https in front
         res.redirect(301, 'https://' + data.originalURL);
@@ -77,9 +85,23 @@ mongoose.connect(process.env.MONGODB_URI, function(err, db){
       }
     }); //end findOne
   }); //end app.get
-    
+  
+  function prependHttp(url) {
+    let fullURL = url;
+    //if it does not pass the regex test then prepend http
+    if (!httpRegex.test(url)) {
+      fullURL = 'http://'.concat(url);
+    }
+    return fullURL;
+  } // end function prependHttp
 }); //end connect
 
-var listener = app.listen(port, () =>{
-  console.log('Your app is listening on port ' + listener.address().port);
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.listen(port, function () {
+  console.log('Node.js listening on port ' +port  );
 });
